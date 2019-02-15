d41b80a876bf78a0158cf074a0791001e03ad02a
troy <sabotuer99@gmail.com>
2019-02-14 22:28:39 -0800

    finished Pascal

--- week1/recfun/src/main/scala/recfun/Main.scala
+++ week1/recfun/src/main/scala/recfun/Main.scala
@@ -11,41 +11,52 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = {
-      def pascalIter(ci: Int, ri: Int, memo: Map[String,Int]): Int = {
-        if (ci < 0 || ci > ri || ri < 0 ) return 0
-        if (ci == 0 || ci == ri) return 1
-        if (ci == c && ri == r) memo(makeKey(ci - 1, ri - 1)) + memo(makeKey(ci, ri - 1))
-      }
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): BigInt = {
+    def pascalIter(ci: Int, ri: Int, memo: Map[String, BigInt]): BigInt = {
+      val key = makeKey(ci, ri)
+      if (memo.contains(key)) return memo(key)
 
-      def makeKey(c: Int, r: Int): String = {
-        c.toString + "_" + r.toString
-      }
+      val leftKey = makeKey(ci - 1, ri - 1)
+      val rightKey = makeKey(ci, ri - 1)
+      val left = if (memo.contains(leftKey)) memo(leftKey) else BigInt.apply(0)
+      val right = if (memo.contains(rightKey)) memo(rightKey) else BigInt.apply(0)
+      val thisCell = left + right
+      val newMemo = if (ci == 0 || ci == ri) memo + (key -> BigInt.apply(1)) else memo + (key -> thisCell)
 
+      if (ci == c && ri == r) return newMemo(key)
 
-      pascalIter(c - r, 0, Map())
+      val nextRi = if (ci == 0) ri + 1 else ri
+      val nextCi = if (ci == 0) r + c - ri else ci - 1
+      pascalIter(nextCi, nextRi, newMemo)
     }
-  
-  /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = {
-      def balIter(chars: List[Char], score: Int): Boolean = {
-        if (chars.isEmpty) return true
-        val increment =
-          if (chars.head == '(') 1
-          else if (chars.head == ')') -1
-          else 0
-        (score + increment) >= 0 && balIter(chars.tail, score + increment)
-      }
 
-      balIter(chars,0)
+    def makeKey(c: Int, r: Int): String = {
+      c.toString + "_" + r.toString
     }
-  
-  /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+
+    pascalIter(r + c, 0, Map())
   }
+
+  /**
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = {
+    def balIter(chars: List[Char], score: Int): Boolean = {
+      if (chars.isEmpty) return true
+      val increment =
+        if (chars.head == '(') 1
+        else if (chars.head == ')') -1
+        else 0
+      (score + increment) >= 0 && balIter(chars.tail, score + increment)
+    }
+
+    balIter(chars, 0)
+  }
+
+  /**
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
