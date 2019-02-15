ea8573114d3979bd53a912817b0e7d0258cdffea
troy <sabotuer99@gmail.com>
2019-02-14 22:34:34 -0800

    additional test for Pascal

--- week1/recfun/src/test/scala/recfun/PascalSuite.scala
+++ week1/recfun/src/test/scala/recfun/PascalSuite.scala
@@ -1,6 +1,8 @@
 package recfun
 
 import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class PascalSuite extends FunSuite {
@@ -21,6 +23,10 @@ class PascalSuite extends FunSuite {
 
   test("pascal: col=9,row=13") {
     assert(pascal(9, 13) === 715)
+  }
+
+  test("pascal: col=35,row=78") {
+    assert(pascal(35, 78) === BigInt.apply("18139751154740212157160"))
   }
 
 }
