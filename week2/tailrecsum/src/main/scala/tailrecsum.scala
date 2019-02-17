ec2f2f1533ede7b071f97649c52f2f2a8ba15025
troy <sabotuer99@gmail.com>
2019-02-17 11:12:06 -0800

    first week2 sample problem definition

---
+++ week2/tailrecsum/src/main/scala/tailrecsum.scala
@@ -0,0 +1,9 @@
+class tailrecsum {
+  def sum(f: Int => Int)(a: Int, b: Int): Int = {
+    def loop(a: Int, acc: Int): Int = {
+      if (???) ???
+      else loop(???, ???)
+    }
+    loop(???, ???)
+  }
+}
